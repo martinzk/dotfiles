4e3710fb98158c29c0eac676541b293318ca4307
Martin Zimmer Kristensen <martinzimmerkristensen@gmail.com>
2016-10-31 13:44:38 +0100

    article

--- article/auto/article.el
+++ article/auto/article.el
@@ -6,12 +6,12 @@
    (TeX-add-to-alist 'LaTeX-provided-package-options
                      '(("inputenc" "utf8") ("fontenc" "T1") ("ulem" "normalem")))
    (add-to-list 'LaTeX-verbatim-environments-local "semiverbatim")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "latex2e"
@@ -32,9 +32,9 @@
     "capt-of"
     "hyperref")
    (LaTeX-add-labels
-    "sec:orgheadline10"
-    "sec:orgheadline21"
-    "sec:orgheadline27"
-    "sec:orgheadline29"))
+    "sec:orgheadline11"
+    "sec:orgheadline22"
+    "sec:orgheadline28"
+    "sec:orgheadline30"))
  :latex)
 
